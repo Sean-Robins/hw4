1b6addd0431eac2022fd4bae023b27058bafa16f
Sean Robins <sean.robins@kellogg.northwestern.edu>
2025-03-17 03:40:41 +0000

    updates

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,14 +4,19 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry["user_id"] = @current_user["id"]
-    @entry.uploaded_image.attach(params["uploaded_image"])
-    @entry.save
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry["user_id"] = @current_user["id"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
+      @entry.save
+    else
+      flash["notice"] = "Please Login first."
+    end
     redirect_to "/places/#{@entry["place_id"]}"
   end
 
